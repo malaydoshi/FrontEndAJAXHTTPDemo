2c5973317e6f0fed7ee0aff73f7d12ebb5893cf4
Malay Doshi <malay.doshi@atidan.com>
2020-07-17 12:47:19 +0530

    Added Comments & Regions

--- FrontEndAJAXHTTPDemo/Startup.cs
+++ FrontEndAJAXHTTPDemo/Startup.cs
@@ -19,24 +19,29 @@ namespace FrontEndAJAXHTTPDemo
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            #region CORS
             var origins = Configuration["AllowedOrigins"].Split(";");
             services.AddCors(c =>
             {
                 c.AddPolicy("AllowOrigin", options => options.WithOrigins(origins).AllowAnyHeader().AllowAnyMethod());
-            });
+            }); 
+            #endregion
+
             services.AddControllers();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+            //Get Origins & Use between Routing & Authorization
+            var origins = Configuration["AllowedOrigins"].Split(";");
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
             }
 
             app.UseRouting();
-
+            app.UseCors(options => options.WithOrigins(origins));
             app.UseAuthorization();
 
             app.UseEndpoints(endpoints =>
