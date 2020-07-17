b6094f7ff8af358a2cad127e0a5bc9fb913f58af
Malay Doshi <malay.doshi@atidan.com>
2020-07-17 12:42:04 +0530

    Added Cors Removed extra usings

--- FrontEndAJAXHTTPDemo/Controllers/WeatherForecastController.cs
+++ FrontEndAJAXHTTPDemo/Controllers/WeatherForecastController.cs
@@ -1,12 +1,14 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Threading.Tasks;
+using Microsoft.AspNetCore.Cors;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 
 namespace FrontEndAJAXHTTPDemo.Controllers
 {
+    
+    [EnableCors("AllowOrigin")]
     [ApiController]
     [Route("[controller]")]
     public class WeatherForecastController : ControllerBase
